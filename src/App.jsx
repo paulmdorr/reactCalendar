64ae186e21d6e85a4a934d979522c3bc36ed6227
Paul M Dorr <paul.mdorr@gmail.com>
2019-05-25 16:51:39 -0300

    Add basic month grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { startOfMonth, endOfMonth, eachDay, subDays, addDays } from 'date-fns'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
 
-const App = () => (
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  item: {
+    width: '14%',
+  },
+})
+
+function getDays(date) {
+  const firstDay = startOfMonth(date)
+  let extraBeforeDays = firstDay.getDay()
+  let daysBefore = []
+  const lastDay = endOfMonth(date)
+  let extraAfterDays = 6 - lastDay.getDay()
+  let daysAfter = []
+
+  while(extraBeforeDays > 0) {
+    daysBefore.push(subDays(firstDay, extraBeforeDays--))
+  }
+
+  while(extraAfterDays > 0) {
+    daysAfter.push(addDays(lastDay, extraAfterDays--))
+  }
+
+  return [
+    ...daysBefore,
+    ...eachDay(startOfMonth(date), endOfMonth(date)),
+    ...daysAfter,
+  ]
+}
+
+const App = ({classes}) => (
   <React.Fragment>
     <Helmet>
-      <title>Home Page</title>
+      <title>React Calendar - Home Page</title>
     </Helmet>
-    <div>
-      Test
-    </div>
+    <Grid container spacing={3}>
+      {getDays(Date.now()).map(day =>
+        <Grid item className={classes.item}>
+          <Paper className={classes.paper}>
+            {day.getDate()}
+          </Paper>
+        </Grid>
+      )}
+    </Grid>
   </React.Fragment>
 )
 
-export default App
+export default withStyles(styles)(App)
