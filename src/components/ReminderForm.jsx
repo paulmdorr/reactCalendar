e07906c56ab62e131a4aebece3c3d0f59ee5635c
Paul M Dorr <paul.mdorr@gmail.com>
2019-05-27 00:58:01 -0300

    Save reminders to global state

--- src/components/ReminderForm.jsx
+++ src/components/ReminderForm.jsx
@@ -1,8 +1,9 @@
 // @flow
-import React from 'react'
+import React, { useState } from 'react'
 import { Popover, TextField, Button, Typography, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { format } from 'date-fns'
+import { StateConsumer, SAVE_REMINDER } from '../App'
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -32,40 +33,66 @@ const ReminderForm = ({ anchorEl, handleClose, date }: Props) => {
   const isOpen = !!anchorEl
   const id = isOpen ? 'simple-popover' : null
   const classes = useStyles()
+  const [reminderText, setReminderText] = useState()
+
+  const handleSave = dispatch => {
+    dispatch({
+      type: SAVE_REMINDER,
+      id: date.getTime(),
+      reminder: reminderText,
+    })
+    handleClose()
+  }
+
+  const popoverProps = {
+    id,
+    open: isOpen,
+    anchorEl,
+    onClose: handleClose,
+    anchorOrigin: {
+      vertical: 'center',
+      horizontal: 'left',
+    },
+    transformOrigin: {
+      vertical: 'center',
+      horizontal: 'right',
+    },
+    classes: {
+      paper: classes.paper,
+    },
+  }
+
+  const textFieldProps = {
+    id: 'reminder',
+    placeholder: 'Write a reminder',
+    autoFocus: true,
+    className: classes.textField,
+    onChange: (event) => setReminderText(event.target.value),
+    inputProps: { maxLength: 30 },
+  }
+
+  const buttonProps = {
+    color: 'primary',
+    variant: 'contained',
+    className: classes.button,
+  }
 
   return (
-    <Popover
-      id={id}
-      open={isOpen}
-      anchorEl={anchorEl}
-      onClose={handleClose}
-      anchorOrigin={{
-        vertical: 'center',
-        horizontal: 'left',
-      }}
-      transformOrigin={{
-        vertical: 'center',
-        horizontal: 'right',
-      }}
-      classes={{
-        paper: classes.paper,
-      }}
-    >
-      <TextField
-        id="reminder"
-        placeholder="Write a reminder"
-        autoFocus
-        className={classes.textField}
-      />
-      <Typography className={classes.date}>
-        Date: {date && format(date, 'MMM DD, YYYY')}
-      </Typography>
-      <Box className={classes.box}>
-        <Button color="primary" variant="contained" className={classes.button}>
-          Save
-        </Button>
-      </Box>
-    </Popover>
+    <StateConsumer>
+      {({ dispatch }) => (
+        <Popover {...popoverProps} >
+          <TextField {...textFieldProps} />
+          <Typography className={classes.date}>
+            Date: {date && format(date, 'MMM DD, YYYY')}
+          </Typography>
+          <Box className={classes.box}>
+            <Button {...buttonProps} onClick={() => handleSave(dispatch)}>
+              Save
+            </Button>
+          </Box>
+        </Popover>
+      )}
+    </StateConsumer>
   )
 }
 
